f2988f72b2ff4b1168e290830780753df6ea4700
Paul Glavich <glav@theglavs.com>
2010-12-24 08:20:03 +1100

    Added constants for PageIDs and associated with nav styles

---
+++ Journaliser/Models/Pages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Journaliser.Models
+{
+    public static class Pages
+    {
+        public const string SignIn = "signin";
+        public const string Register = "register";
+        public const string Home = "home";
+        public const string About = "about";
+        public const string AddToJournal = "addtojournal";
+    }
+}

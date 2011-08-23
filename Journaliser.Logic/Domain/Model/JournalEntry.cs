e7de9897c32baebf61eb85f80d2813fd0b900b28
Paul Glavich <glav@theglavs.com>
2011-08-24 07:37:51 +1000

    clean up

--- Journaliser.Logic/Domain/Model/JournalEntry.cs
+++ Journaliser.Logic/Domain/Model/JournalEntry.cs
@@ -7,11 +7,26 @@ namespace Journaliser.Logic.Domain.Model
 {
     public class JournalEntry : BaseDocument
     {
+		public JournalEntry()
+		{
+			//SomeStuff = new SomeData();
+		}
         public string Title { get; set; }
         public JournalEntryVisibility Visibility {get; set;}
         public DateTime? LastModifiedDate { get; set; }
         public string BodyText { get; set; }
-
+		//public SomeData SomeStuff { get; set; }
 		//public string NewField { get; set; }
     }
+
+	public class SomeData
+	{
+		public SomeData()
+		{
+			Stuff = "this";
+			NumberStuff = 2;
+		}
+		public string Stuff { get; set; }
+		public int NumberStuff { get; set; }
+	}
 }
